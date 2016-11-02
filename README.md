4ec259d9dca7c822fd4d4245bbcc0fd4dee5182c
mithisingh <mithileshgpsingh@gmail.com>
2016-11-03 00:28:57 +0530

    generic PHP cookbook

---
+++ README.md
@@ -0,0 +1,1 @@
+Generic cookbook for PHP installation at Linux server.
---
+++ metadata.rb
@@ -0,0 +1,6 @@
+
+name 'PHP'
+maintainer_email 'mithileshgpsingh@gmail.com'
+version '0.1.0'
+supports 'redhat', '>= 5.0'
+supports 'centos', '>= 5.0'
